8742c0fffd28f8fee5e3be69cad15a1bd75a87fe
akash <akash.ry@somaiya.edu>
2021-05-07 20:32:07 +0530

    improvements and bug fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from './containers/Checkout/Checkout'
 import { connect } from 'react-redux'
 
 import { Route, Switch, Redirect } from 'react-router-dom'
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout';
 
 import { useEffect } from 'react'
 
 import * as actions from './store/actions/index'
+import asyncComponent from './hoc/asyncComponent/asyncComponent'
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout')
+})
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth')
+})
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders')
+})
 
 function App(props) {
 
@@ -21,7 +29,7 @@ function App(props) {
 
   let routes = (
     <Switch>
-      <Route path="/auth" component={Auth} />
+      <Route path="/auth" component={asyncAuth} />
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to="/" />
     </Switch>
@@ -30,10 +38,12 @@ function App(props) {
   if (props.isAuthenticated) {
     routes = (
       <Switch>
-        <Route path="/checkout" component={Checkout} />
+        <Route path="/checkout" component={asyncCheckout} />
 
+        <Route path="/orders" component={asyncOrders} />
         <Route path="/logout" component={Logout} />
-        <Route path="/orders" component={Orders} />
+        <Route path="/auth" component={asyncOrders} />
+
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import Input from '../../../components/UI/Input/Input'
 import { connect } from 'react-redux'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
+import { updateObject, checkValidity } from '../../../shared/utility'
 
 class ContactData extends Component {
   state = {
@@ -112,39 +113,18 @@ class ContactData extends Component {
 
   }
 
-  checkValidity(value, rules) {
-    let isValid = true
-
-    // if(!rules){
-    //   return true
-    // }
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
-    }
-
-    return isValid
-  }
 
   inputChangedHandler = (e, inputIdentifier) => {
-    const updatedOrderForm = {
-      ...this.state.orderForm
-    }
-    const updatedFormElement = {
-      ...updatedOrderForm[inputIdentifier]
-    }
 
-    updatedFormElement.value = e.target.value
-    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
-    updatedFormElement.touched = true
-    updatedOrderForm[inputIdentifier] = updatedFormElement
+    const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+      value: e.target.value,
+      valid: checkValidity(e.target.value, this.state.orderForm[inputIdentifier].validation),
+      touched: true
+    })
+
+    const updatedOrderForm = updateObject(this.state.orderForm, {
+      [inputIdentifier]: updatedFormElement
+    })
 
     let formIsValid = true
 
